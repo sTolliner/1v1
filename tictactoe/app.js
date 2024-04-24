5c20c8b621b61087d58fc06bdcfcdfbc0b08d846
Tjiko <Nordberg.william@gmail.com>
2024-04-24 12:54:29 +0200

    jappar mor din

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
 //Filen app.js är den enda ni skall och tillåts skriva kod i.
+const globalObject = require('/servermodules/game-modul.js');
+const fs = require('fs');
+const express = require('express');
+const jsDOM = require('jsdom');
+const cookieParser = require('cookie-parser')
+
+let app = express();
+
+app.listen(3000, function () {
+    console.log(":3")
+} );
+app.use("/static", express.static(__dirname + "/static"));
